d0915b02feb61761d4120bbd379b65557e1cfc86
EinatNoyman <einat2004@gmail.com>
2022-08-12 13:39:05 +0300

    Start tester branch

--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -3,9 +3,8 @@
 //
 
 #include "TCPServer.hpp"
-#include <string>
+
 #include <utility>
-#include <memory>
 #include "KNearestNeighbors.hpp"
 #include "CSVManagement.hpp"
 #include "Point.hpp"
@@ -14,72 +13,71 @@
 using namespace std;
 using namespace CSV;
 using namespace Networking;
-TCPServer::TCPServer(int port,string dataPath) : port(port),_dataPath(std::move(dataPath)){
+
+TCPServer::TCPServer(int port, string dataPath) : port(port), _dataPath(std::move(dataPath)) {
     // creates socket
-    _sock=socket(AF_INET,SOCK_STREAM,0);
-    if(_sock<0){
+    _sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (_sock < 0) {
         perror("Error creating socket");
     }
     // tries to bind the socket to the port.
     struct sockaddr_in sin;
-    memset(&sin,0,sizeof(sin));
-    sin.sin_family=AF_INET;
-    sin.sin_addr.s_addr=INADDR_ANY;
-    sin.sin_port=htons(this->port);
-    if(bind(_sock,(struct sockaddr *)&sin,sizeof(sin))<0){
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(this->port);
+    if (bind(_sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("Error binding socket");
     }
     // starts listening to the socket.
-    if(listen(_sock,5)<0){
+    if (listen(_sock, 5) < 0) {
         perror("Error listening to a socket");
     }
 }
 
 void TCPServer::handleClient() {
     // K of the K nearest neighbors algorithm.
-    const int K=3;
+    const int K = 3;
     // accepts the client.
     struct sockaddr_in client_sin;
-    unsigned int addr_len=sizeof(client_sin);
-    int client_sock=accept(_sock,(struct sockaddr*)&client_sin,&addr_len);
-    if(client_sock<0){
+    unsigned int addr_len = sizeof(client_sin);
+    int client_sock = accept(_sock, (struct sockaddr *) &client_sin, &addr_len);
+    if (client_sock < 0) {
         perror("Error accepting client");
     }
     // receives messages from the client.
     char buffer[4096];
-    string final{};
     int expected_data_len = sizeof(buffer);
-    int read_bytes = (int)recv(client_sock, buffer, expected_data_len, 0);
+    int read_bytes = (int) recv(client_sock, buffer, expected_data_len, 0);
     if (read_bytes == 0) {
         cout << "Connection terminated" << endl;
         close(_sock);
         return;
-    }
-    else if (read_bytes < 0) {
+    } else if (read_bytes < 0) {
         perror("Error reading client message");
-    }
-    else {
+    } else {
         // processing of client data.
         // client sends the server the input path.
         string inputPath(buffer);
         // classifies the data using euclidian distance.
-        CSVManagement csvm(_dataPath,inputPath);
-        vector<vector<Point>> data=csvm.getClassifiedData();
+        CSVManagement csvm(_dataPath, inputPath);
+        vector<vector<Point>> data = csvm.getClassifiedData();
         EuclideanDistance euclideanDistance{};
-        vector<string> classifiedData=KNearestNeighbors::classifyData(K,euclideanDistance,csvm.getClassifiedData(),csvm.getUnclassifiedData());
+        vector<string> classifiedData = KNearestNeighbors::classifyData(K, euclideanDistance, csvm.getClassifiedData(),
+                                                                        csvm.getUnclassifiedData());
+        string final;
         //combines the classified data into one string (with ' ' (space) as separation between classifications).
-        for(int i=0;i<classifiedData.size();i++){
-            if(i==0){
-                final.append(classifiedData.at(0));
-            }
-            else{
-                final.append(' '+classifiedData.at(i));
+        for (int i = 0; i < classifiedData.size(); i++) {
+            if (i == 0) {
+                final += classifiedData.at(0);
+            } else {
+                final += ' ' + classifiedData.at(i);
             }
         }
 
     }
     //sends the combined string of the classifications.
-    int sent_bytes =(int) send(client_sock,final.c_str(), read_bytes, 0);
+    int sent_bytes = (int) send(client_sock, buffer, read_bytes, 0);
     if (sent_bytes < 0) {
         perror("Error sending to client");
     }
